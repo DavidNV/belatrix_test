6dd6a80374818a17fd9794a4b40651e88d70e245
Orferus <david.nvalderrama@gmail.com>
2018-03-20 08:08:31 -0500

    Add mising test cases

--- user_spec.rb
+++ user_spec.rb
@@ -4,24 +4,43 @@ require './user'
 RSpec.describe User do
   describe ".new" do
     context "with block params" do
-      it "should create the object with the valid attributes" do
-        new_user = User.new do |user|
-          user.name = "David"
-          user.last_name = "Valderrama"
-          user.another_method = "Hi"
+      context "the block exists" do
+        it "should create the object with the valid attributes" do
+          new_user = User.new do |user|
+            user.name = "David"
+            user.last_name = "Valderrama"
+            user.another_method = "Hi"
+          end
+          expect(new_user.name).to eq("David")
+          expect(new_user.last_name).to eq("Valderrama")
+          expect(new_user.respond_to?(:another_method)).to be_falsey
         end
-        expect(new_user.name).to eq("David")
-        expect(new_user.last_name).to eq("Valderrama")
-        expect(new_user.respond_to?(:another_method)).to be_falsey
+      end
+      context "the block is empty" do
+        it "should create just the object" do
+          new_user = User.new {|user| user}
+          expect(new_user.name).to be_nil
+          expect(new_user.last_name).to be_nil
+        end
       end
     end
-    let(:params) { { name: "David", last_name: "Valderrama", another_key: "Hi" } }
     context "with hash params" do
-      it "should create the object with the valid attributes" do
-        new_user = User.new(params)
-        expect(new_user.name).to eq("David")
-        expect(new_user.last_name).to eq("Valderrama")
-        expect(new_user.respond_to?(:another_method)).to be_falsey
+      let(:params) { { name: "David", last_name: "Valderrama", another_key: "Hi" } }
+      context "the params exists" do
+        it "should create the object with the valid attributes" do
+          new_user = User.new(params)
+          expect(new_user.name).to eq("David")
+          expect(new_user.last_name).to eq("Valderrama")
+          expect(new_user.respond_to?(:another_method)).to be_falsey
+        end
+      end
+      context "the params are empty" do
+        it "should create just the object" do
+          new_user = User.new({})
+          expect(new_user.name).to be_nil
+          expect(new_user.last_name).to be_nil
+          expect(new_user.respond_to?(:another_method)).to be_falsey
+        end
       end
     end
     context "without params" do
