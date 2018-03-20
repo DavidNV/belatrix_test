64d6ac345fa149faf43b1eaeceb5b07976575530
Orferus <david.nvalderrama@gmail.com>
2018-03-19 23:07:56 -0500

    Update base with hash params green spec

--- base.rb
+++ base.rb
@@ -1,27 +1,20 @@
 class Base
   def initialize(at = nil)
-    if at == nil && block_given?
-      yield self
-    end
-    if at != nil
-      assign_attributes(at)
-    end
+    #yield self if block_given?
+    assign_attributes(at)
   end
 
   def assign_attributes(at)
-    if at.empty?
-      return
-    end
-    new_at = stringify_keys(at)
-    _assign_attributes(new_at)
+    return unless at && at.any?
+    _assign_attributes(at)
   end
 
   private
+
   def _assign_attributes(at)
-    at.each do |a|
-      key = a[0]
-      value = a[1]
-      _assign_attribute(key, value)
+    at.each do |method, value|
+      next unless respond_to?(method)
+      _assign_attribute(method.to_s, value)
     end
   end
 
@@ -30,19 +23,4 @@ class Base
     public_send(setter, value)
   end
 
-  def transform_keys(hash)
-    result = {}
-    hash.each_key do |key|
-      s = yield(key)
-      result[s] = hash[key]
-    end
-    return result
-  end
-
-  def stringify_keys(hash)
-    transform_keys(hash) do |k|
-      k.to_s
-    end
-  end
 end
-
